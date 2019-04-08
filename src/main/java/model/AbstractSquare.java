c09c74a3b4404fc8767c97475f6b48d94ec7f362
Michele <miki199714@gmail.com>
2019-04-08 18:31:33 +0200

    add AbstractSquare and Figure

--- src/main/java/model/AbstractSquare.java
+++ src/main/java/model/AbstractSquare.java
@@ -1,11 +1,29 @@
 package model;
 
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
 
 public abstract class AbstractSquare implements Targettable{
     private List<AbstractSquare> adjacent;
     private Room room;
+    private Set<Figure> occupants;
     public abstract void accept(Game game);
+    public abstract void grab(Figure grabber);
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public boolean sees(Room target){
+        return target==room || adjacent.stream().map(AbstractSquare::getRoom).anyMatch(Predicate.isEqual(target)) ;
+    }
+    public boolean sees(AbstractSquare target){
+        return sees(target.getRoom());
+    }
+    public boolean sees(Figure target){
+        return sees(target.getSquare());
+    }
 
 
 }
--- src/main/java/model/Figure.java
+++ src/main/java/model/Figure.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Figure {
+public class Figure implements Targettable{
+    private AbstractSquare square;
+    @Override
+    public void doDamage(Figure dealer) {
+
+    }
+
+    @Override
+    public void doMark(Figure dealer) {
+
+    }
+
+    public AbstractSquare getSquare() {
+        return square;
+    }
+
+
 }
