2e70a6cdf926cece6c01390b2eeba8292696218b
Michele <miki199714@gmail.com>
2019-04-10 14:39:06 +0200

    fix AmmoCube, add test for AmmoCube

--- src/main/java/model/AmmoCube.java
+++ src/main/java/model/AmmoCube.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -9,16 +10,27 @@ public final class AmmoCube {
     public AmmoCube(List<Integer> ammos){
         this.ammos=ammos;
     }
+
     public AmmoCube add(AmmoCube toAdd){
-        return new AmmoCube(IntStream.range(0,Math.min(ammos.size(),toAdd.getAmmos().size())).
-                mapToObj(i->Integer.min(3,ammos.get(i)+toAdd.getAmmos().get(i))).collect(Collectors.toList()));
+        return new AmmoCube(this.rangeAmmos(toAdd).
+                mapToObj(i->ammos.get(i)+toAdd.getAmmos().get(i)).collect(Collectors.toList()));
     }
     public AmmoCube sub(AmmoCube toSub){
-        return new AmmoCube(IntStream.range(0,Math.min(ammos.size(),toSub.getAmmos().size())).
-                mapToObj(i->Integer.max(0,ammos.get(i)-toSub.getAmmos().get(i))).collect(Collectors.toList()));
+        return new AmmoCube(this.rangeAmmos(toSub).
+                mapToObj(i->ammos.get(i)-toSub.getAmmos().get(i)).collect(Collectors.toList()));
     }
 
     public List<Integer> getAmmos() {
         return ammos;
+    }
+    public AmmoCube cap(){
+        return new AmmoCube(ammos.stream().map(x->Integer.min(3,x)).collect(Collectors.toList()));
+    }
+    public boolean greaterThan(AmmoCube other){
+        return this.rangeAmmos(other).filter(i->ammos.get(i)<other.getAmmos().get(i)).count()==0;
+
+    }
+    private IntStream rangeAmmos(AmmoCube other){
+        return IntStream.range(0,Math.min(ammos.size(),other.getAmmos().size()));
     }
 }
--- src/test/java/model/AmmoCubeTest.java
+++ src/test/java/model/AmmoCubeTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AmmoCubeTest {
     @Test
-    void TestSimpleSum(){
+    void testSimpleSum(){
         AmmoCube a= new AmmoCube(new ArrayList<>(Arrays.asList(1,2,3)));
         AmmoCube b= new AmmoCube(new ArrayList<>(Arrays.asList(2,0,0)));
         AmmoCube c = a.add(b);
@@ -17,15 +17,15 @@ class AmmoCubeTest {
         assertEquals(r,c.getAmmos());
     }
     @Test
-    void TestSumLimit(){
+    void testSumLimit(){
         AmmoCube a= new AmmoCube(new ArrayList<>(Arrays.asList(1,2,3)));
         AmmoCube b= new AmmoCube(new ArrayList<>(Arrays.asList(3,0,0)));
         AmmoCube c = a.add(b);
-        ArrayList<Integer> r= new ArrayList<>(Arrays.asList(3,2,3));
+        ArrayList<Integer> r= new ArrayList<>(Arrays.asList(4,2,3));
         assertEquals(r,c.getAmmos());
     }
     @Test
-    void TestSimpleSub(){
+    void testSimpleSub(){
         AmmoCube a= new AmmoCube(new ArrayList<>(Arrays.asList(1,2,3)));
         AmmoCube b= new AmmoCube(new ArrayList<>(Arrays.asList(1,0,0)));
         AmmoCube c = a.sub(b);
@@ -33,12 +33,33 @@ class AmmoCubeTest {
         assertEquals(r,c.getAmmos());
     }
     @Test
-    void TestSubLimit(){
+    void testSubLimit(){
         AmmoCube a= new AmmoCube(new ArrayList<>(Arrays.asList(1,2,3)));
         AmmoCube b= new AmmoCube(new ArrayList<>(Arrays.asList(3,0,0)));
         AmmoCube c = a.sub(b);
-        ArrayList<Integer> r= new ArrayList<>(Arrays.asList(0,2,3));
+        ArrayList<Integer> r= new ArrayList<>(Arrays.asList(-2,2,3));
         assertEquals(r,c.getAmmos());
+    }
+    @Test
+    void testCap(){
+        AmmoCube a= new AmmoCube(new ArrayList<>(Arrays.asList(1,2,3)));
+        AmmoCube b= new AmmoCube(new ArrayList<>(Arrays.asList(3,0,0)));
+        AmmoCube c = a.add(b);
+        ArrayList<Integer> r= new ArrayList<>(Arrays.asList(4,2,3));
+        c=c.cap();
+        r= new ArrayList<>(Arrays.asList(3,2,3));
+        assertEquals(r,c.getAmmos());
+    }
+    @Test
+    void testGreater(){
+        AmmoCube a= new AmmoCube(new ArrayList<>(Arrays.asList(1,2,3)));
+        AmmoCube b= new AmmoCube(new ArrayList<>(Arrays.asList(3,0,0)));
+        assertFalse(b.greaterThan(a));
+        assertFalse(a.greaterThan(b));
+        a= new AmmoCube(new ArrayList<>(Arrays.asList(1,2,3)));
+        b= new AmmoCube(new ArrayList<>(Arrays.asList(1,0,0)));
+        assertFalse(b.greaterThan(a));
+        assertTrue(a.greaterThan(b));
     }
 
 }
