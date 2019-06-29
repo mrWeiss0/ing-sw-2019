6ad942f31f9075b9944b4688d08b1b130bf26b8b
mrWeiss <giorgio2197@gmail.com>
2019-06-29 14:02:28 +0200

    change Action to interface

--- src/main/java/server/controller/Action.java
+++ src/main/java/server/controller/Action.java
@@ -1,16 +1,8 @@
 package server.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class Action {
-    private final GameController.State[] states;
-
-    public Action(GameController.State... states) {
-        this.states = states;
-    }
-
-    public List<GameController.State> getStates() {
-        return Arrays.asList(states);
-    }
+@FunctionalInterface
+public interface Action {
+    List<GameController.State> getStates();
 }
