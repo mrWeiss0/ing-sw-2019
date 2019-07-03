77437e0731f542b3c12f19501538d3f090651f59
Guido Rolle <37444585+ilgrandegalileo@users.noreply.github.com>
2019-07-03 14:27:35 +0200

    Added and renamed files, modified Play controller

--- src/main/java/client/view/graphic/controllers/Play.java
+++ src/main/java/client/view/graphic/controllers/Play.java
@@ -2,9 +2,26 @@ package client.view.graphic.controllers;
 
 import client.view.View;
 import client.view.graphic.loaders.ImageLoader;
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
 
 public class Play implements View {
     private ImageLoader loader = new ImageLoader();
+
+    @FXML ImageView yourWeapon0;
+    @FXML Button yourWeapon0fireMode0;
+    @FXML Button yourWeapon0fireMode1;
+    @FXML Button yourWeapon0fireMode2;
+    @FXML ImageView yourWeapon1;
+    @FXML Button yourWeapon1fireMode1;
+    @FXML Button yourWeapon1fireMode2;
+    @FXML Button yourWeapon1fireMode3;
+    @FXML ImageView yourWeapon2;
+    @FXML Button yourWeapon2fireMode1;
+    @FXML Button yourWeapon2fireMode2;
+    @FXML Button yourWeapon2fireMode3;
 
 
 }
